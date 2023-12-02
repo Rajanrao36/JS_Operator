d028418463648fa6376da4d29c40a420654b129d
Rajan Rao <72188468+Rajanrao36@users.noreply.github.com>
2023-12-02 22:14:00 +0530

    Add files via upload

---
+++ Operator.js
@@ -0,0 +1,89 @@
+//Arithematic Operators
+//addition
+
+let a = 6 
+let b = 8 
+let c = a+b;
+    console.log(c) //14
+
+//.....................
+//substraction
+
+let a1 = 100; b1 = 35;
+    console.log(a1-b1); //65
+
+//.....................
+//multiplication
+
+let a3 = 12; b3 = 12;
+    console.log(a3*b3); //144
+
+//.....................
+//division
+
+let a4 = 56; b4 = 14;
+    console.log(a4/b4); //4
+
+//.....................
+// Remainder Operator
+
+let a5 = 93; b5 = 3;
+    console.log(a5%b5); //0
+
+//.....................    
+// Assignment Operator
+// increment (pre-fix)
+let a6 = 8;
+    console.log(++a6); //9
+
+let b6 = 5;
+    console.log(--b6); //4
+
+// increment (post-fix)
+
+let aa = 9;
+    console.log(aa--); //9
+
+let bb = 7;
+    console.log(bb--); //7
+
+//.....................
+
+let a7 = 6;
+    a7 *= 2;
+    console.log(a7); //12
+    
+let b7 = 21;
+    b7 /= 3
+    console.log(b7); //7
+    
+//.................
+//Unary Operators
+
+let A = "17"; // string
+    B = +A;
+    console.log("B :", B); // 17
+    console.log(typeof B); //type number
+
+let C = 14;
+    D = -C;
+    console.log("C :", c); //14
+    console.log(typeof C); //type number
+
+let E = true;
+    console.log(!E);
+
+let F = false; 
+    console.log(!F);
+
+//....................
+//Comparison Operators
+
+let X = 85;
+Y = 105;
+console.log(X == Y); //False
+console.log(X != Y); //True
+console.log(X > Y); //False
+console.log(X < Y); //True
+console.log(X >= Y); //False
+console.log(X <= Y); //True    
